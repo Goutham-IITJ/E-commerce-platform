e0d30e76889f4939f3e9cc97dd5c2abbe3801fd1
Goutham-IITJ <b23ee1024@iitj.ac.in>
2025-06-10 12:19:19 +0530

    About us page updated

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 const about = () => {
   return (
     <div>
@@ -22,9 +23,21 @@ const about = () => {
         <Title text1={'WHY '} text2={'CHOOSE US'}/>
       </div>
 
-      <div>
-        
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent quality standars</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience:</b>
+          <p className='text-gray-600'>With our user friendly interface and hassle-free ordering process, shopping has becom easier</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional customer service:</b>
+          <p className='text-gray-600'>Our team of dedicated Professional is here to assist you the way, ensuring your satisfaction is our top priority.</p>
+        </div>
       </div>
+      <NewsletterBox/>
     </div>
   )
 }
