0b2c8748b6f67ae4649582722456b7aeb96f288d
Goutham-IITJ <b23ee1024@iitj.ac.in>
2025-06-12 12:35:07 +0530

    add product logic

---
+++ backend/controllers/productController.js
@@ -0,0 +1,62 @@
+import { response } from "express";
+import {v2 as cloudinary} from "cloudinary";
+import productModel from "../models/productModel.js";
+
+//function for adding product
+const addProduct = async(req, res)=>{
+    try {
+        const {name, description, category, subCategory, sizes, price, bestseller} = req.body;
+        const image1 = req.files.image1 && req.files.image1[0]
+        const image2 = req.files.image2 && req.files.image2[0]
+        const image3 = req.files.image3 && req.files.image3[0]
+        const image4 = req.files.image4 && req.files.image4[0]
+
+        const images = [image1, image2, image3, image4].filter((item)=> item !== undefined)
+
+        let imagesURL = await Promise.all(
+            images.map( async(item) =>{
+                let result = await cloudinary.uploader.upload(item.path, {resource_type : 'image'});
+                return result.secure_url;
+            })
+        )
+
+        const productData = {
+            name,
+            description,
+            category,
+            price:Number(price),
+            subCategory,
+            bestseller: bestseller === "true" ? true : false,
+            sizes: JSON.parse(sizes),
+            image: imagesURL,
+            date: Date.now()
+        }
+
+        console.log(productData);
+        const product = new productModel(productData)
+        await product.save()
+        res.json({message: "Product added successfully"})
+        
+    } catch (error) {
+        console.log(error)
+        res.json({success:'false',message:error.message})
+    }
+}
+
+//function for list products
+const listProducts = async (req,res) => {
+    
+}
+
+//function to remove products 
+const removeProducts = async (req,res) => {
+    
+}
+
+//function for single product info
+const singleProducts = async (req,res) => {
+    
+}
+
+
+export {listProducts, addProduct, removeProducts, singleProducts}
