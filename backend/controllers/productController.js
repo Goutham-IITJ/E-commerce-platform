739daeabb8df05aed59be99e05aae44e0f7f908b
Goutham-IITJ <b23ee1024@iitj.ac.in>
2025-06-13 15:19:30 +0530

    all product api's added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -46,15 +46,41 @@ const addProduct = async(req, res)=>{
 //function for list products
 const listProducts = async (req,res) => {
     
+    try {
+        const products = await productModel.find({});
+        res.json({success:true, products});
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:'false',message:error.message})
+    }
 }
 
 //function to remove products 
 const removeProducts = async (req,res) => {
-    
+    try {
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success:true, message: "Product removed successfully"});
+
+    } catch (error) {
+         console.log(error)
+        res.json({success:'false',message:error.message})
+    }
 }
 
 //function for single product info
 const singleProducts = async (req,res) => {
+
+    try {
+        
+        const {productId} = req.body
+        const product = await productModel.findById(productId)
+        res.json({success:true, product});
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:'false',message:error.message})
+    }
     
 }
 
