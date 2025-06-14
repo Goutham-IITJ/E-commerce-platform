9235647bbe0188c61268b59794e3a8e6954099c2
Goutham-IITJ <b23ee1024@iitj.ac.in>
2025-06-14 14:24:26 +0530

    updated

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Sidebar from "./components/Sidebar";
+import {Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
+    <div >
     <>
       <Navbar/>
+      <hr />
+      <div className=' flex w-full'>
+        <Sidebar/>
+        <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+              <Routes>
+                <Route path='/add' element= {<Add/>} />
+                <Route path='/list' element= {<List/>} />
+                <Route path='/orders' element= {<Orders/>} />
+              </Routes>
+        </div>
+      </div>
     </>
+    </div>
   )
 }
 
